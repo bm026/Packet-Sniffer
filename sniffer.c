83d0153034b486f78b8fbc05770daffec03999cc
bm026 <bt12753@my.bristol.ac.uk>
2016-04-26 22:41:45 +0100

    log style, only monitor incoming packets

--- sniffer.c
+++ sniffer.c
@@ -45,48 +45,60 @@ void caught_packet(u_char *user_args, const struct pcap_pkthdr *cap_header, cons
   const struct tcp_hdr *tcp_header;
   int tcp_header_length, total_header_size, packet_data_length;
   struct in_addr inet_ip;
+  struct in_addr inet_ip_this;
 
   //printf("=== Got a %d byte packet ===\n", cap_header->len);
-  time_t t = time(NULL);
-  struct tm tm = *localtime(&t);
-  char time_string[30];
-
-  sprintf(time_string, "%d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-  printf("%s\n", time_string);
 
   // get IP addresses of packet
   const u_char *ip_hdr_start = packet + ETHER_HDR_LEN;
   ip_header = (const struct ip_hdr *) ip_hdr_start;
 
   // print source and destination IPs
-  inet_ip.s_addr = ip_header->ip_src_addr;
+  /*inet_ip.s_addr = ip_header->ip_src_addr;
   printf("Source:\t\t%s\n", inet_ntoa(inet_ip));
   inet_ip.s_addr = ip_header->ip_dest_addr;
-  printf("Destination:\t%s\n", inet_ntoa(inet_ip));
+  printf("Destination:\t%s\n", inet_ntoa(inet_ip));*/
 
-  // get length of TCP header
-  const u_char *tcp_hdr_start = packet + ETHER_HDR_LEN + sizeof(struct ip_hdr);
-  tcp_header = (const struct tcp_hdr *) tcp_hdr_start;
-  tcp_header_length = 4 * tcp_header->tcp_offset;
+  // only logs packets with destination 192.168.1.35
+  if (inet_aton("192.168.1.35", &inet_ip_this) == 0) fatal("in IP conversion");
+  if (inet_ip_this.s_addr == ip_header->ip_dest_addr) {
 
-  // get data size
-  total_header_size = ETHER_HDR_LEN + sizeof(struct ip_hdr) + tcp_header_length;
-  packet_data_length = cap_header->len - total_header_size;
+    // create timestamp
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    char time_string[30];
+    sprintf(time_string, "%d-%02d-%02d %02d:%02d:%02d >", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    printf("%s\t", time_string);
+    
+    // get source IP address
+    inet_ip.s_addr = ip_header->ip_src_addr;
+    printf("%s\t", inet_ntoa(inet_ip));
 
-  // if data, print size
-  if (packet_data_length > 0) {
-    printf("Data:\t\t%d bytes\n\n", packet_data_length);
+    // get length of TCP header
+    const u_char *tcp_hdr_start = packet + ETHER_HDR_LEN + sizeof(struct ip_hdr);
+    tcp_header = (const struct tcp_hdr *) tcp_hdr_start;
+    tcp_header_length = 4 * tcp_header->tcp_offset;
+
+    // get data size
+    total_header_size = ETHER_HDR_LEN + sizeof(struct ip_hdr) + tcp_header_length;
+    packet_data_length = cap_header->len - total_header_size;
+
+    // if data, print size
+    if (packet_data_length > 0) {
+      //printf("Data:\t\t%d bytes\n\n", packet_data_length);
+      printf("%d bytes\t", packet_data_length);
+    }
+    // if no data, print flags
+    else {
+      printf("No data. Flags: ");
+      if (tcp_header->tcp_flags & TCP_FIN) printf("FIN ");
+      if (tcp_header->tcp_flags & TCP_SYN) printf("SYN ");
+      if (tcp_header->tcp_flags & TCP_RST) printf("RST ");
+      if (tcp_header->tcp_flags & TCP_PUSH) printf("PUSH ");
+      if (tcp_header->tcp_flags & TCP_ACK) printf("ACK ");
+      if (tcp_header->tcp_flags & TCP_URG) printf("URG");
+      //printf("\n\n");
+    }
+    printf("\n");
   }
-  // if no data, print flags
-  else {
-    printf("No data. Flags:\t");
-    if (tcp_header->tcp_flags & TCP_FIN) printf("FIN ");
-    if (tcp_header->tcp_flags & TCP_SYN) printf("SYN ");
-    if (tcp_header->tcp_flags & TCP_RST) printf("RST ");
-    if (tcp_header->tcp_flags & TCP_PUSH) printf("PUSH ");
-    if (tcp_header->tcp_flags & TCP_ACK) printf("ACK ");
-    if (tcp_header->tcp_flags & TCP_URG) printf("URG");
-    printf("\n\n");
-  }
-
 }
