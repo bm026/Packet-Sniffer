5f2e5a39bea69569996c06e2f2a2b10658aee8c8
bm026 <bt12753@my.bristol.ac.uk>
2016-04-26 16:29:09 +0100

    added fatal and error checked malloc

--- h_sniffer.h
+++ h_sniffer.h
@@ -2,7 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+// prints program usage and then exits
 void usage (char *prog_name) {
   printf("\nUsage:  %s [-i]\n\n", prog_name);
   exit(-1);
+}
+
+// prints fatal error message and then exits
+void fatal (char *message) {
+  char error_message[100];
+  strcpy(error_message, "[!!] Fatal Error ");
+  strncat(error_message, message, 82);
+  perror(error_message);
+  exit(-1);
+}
+
+// an error-checked malloc function
+void *challoc (unsigned int size) {
+  void *ptr;
+  ptr = malloc(size);
+  if (ptr == NULL) {
+    fatal("in challoc() on memory allocation");
+  }
+  return ptr;
 }
